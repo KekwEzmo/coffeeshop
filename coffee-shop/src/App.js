f3278fcc87537b3db1c289eba03e57e07b95b2e6
Aryt3 <Jxkobus@protonmail.com>
2024-04-02 17:49:30 +0000

    Bugfixes

--- coffee-shop/src/App.js
+++ coffee-shop/src/App.js
@@ -6,18 +6,21 @@ import Home from './subpages/home';
 import CheckOut from './subpages/checkout';
 import Overview from './subpages/overview';
 import NotFoundPage from './error_404_page'; 
+import { CartProvider } from './cart';
 
 function App() {
   return (
     <div className="App">
+      <CartProvider>
       <NavBar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/checkout" element={<CheckOut />} />
-        <Route exact path="/overview" element={<Overview />} />
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/checkout" element={<CheckOut/>} />
+        <Route exact path="/overview" element={<Overview/>} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
+      </CartProvider>
     </div>
   );
 }
